16b5e2faff8cd1c35e996a550b85f9de4b7697cf
shiva7671 <shiva8184951@gmail.com>
2025-10-27 16:10:28 +0530

    user data auth

--- UserDataAuth/settings.py
+++ UserDataAuth/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS=[STATIC_DIR]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
